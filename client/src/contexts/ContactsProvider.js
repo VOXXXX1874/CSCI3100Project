42465f1fb4621115d9ea3ae959d0ed922e30a0f9
David Liu <davidliu@Davids-MacBook-Air.local>
2024-04-03 18:08:59 +0800

    front end components part 1

---
+++ client/src/contexts/ContactsProvider.js
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from 'react'
+
+const ContactsContext = React.createContext()
+
+export function useContacts() {
+  return useContext(ContactsContext)
+}
+
+export function ContactsProvider({ children }) {
+  const [contacts, setContacts] = useState('contacts', [])
+
+  function createContact(id, name) {
+    setContacts(prevContacts => {
+      return [...prevContacts, { id, name }]
+    })
+  }
+
+  return (
+    <ContactsContext.Provider value={{ contacts, createContact }}>
+      {children}
+    </ContactsContext.Provider>
+  )
+}
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import {PageContext} from '../../components/appPage/pageContext'
 import "./index.css"
 import startGameImage from './startGame.png';
 import leaderBoardImage from './leaderBoard.png';
+import Sidebar from '../../components/Sidebar';
+import {Container, Col, Row } from 'react-bootstrap'; 
 
 export default function HomePage(){
     const {logout, startGame, modifySettings, pastGame, leaderBoard } = useContext(PageContext);
@@ -30,30 +32,40 @@ export default function HomePage(){
     return(
         <div className="HomePage">
             <h2>This is Home page</h2>
-            <div className="ButtonsContainer">
-                {/* <button className="PageButton" onClick={handleStartGame}>Game</button>
-                <button className="PageButton" onClick={handleSettings}>Settings</button>
-                <button className="PageButton" onClick={handleLogout}>Logout</button> */}
-
-<button className="PageButton" onClick={startGame}>
-          <div className="ButtonContent">
-          <img src={startGameImage} alt="Start Game" style={{ width: '346px', height: '260px' }} />
-            <div className="ButtonText">Start Game</div>
-          </div>
-        </button>
-        <button className="PageButton" onClick={pastGame}>
-          <div className="ButtonContent">
-          <img src={startGameImage} alt="Start Game" style={{ width: '346px', height: '260px' }} />
-            <div className="ButtonText">Replay Past Games</div>
-          </div>
-        </button>
-        <button className="PageButton" onClick={leaderBoard}>
-          <div className="ButtonContent">
-          <img src={leaderBoardImage} alt="Start Game" style={{ width: '346px', height: '270px' }} />
-            <div className="ButtonText">Leader Board</div>
-          </div>
-        </button>
-      </div>
+        <Container>
+          <Row>
+            <Col>
+              <div className="PageChat">
+                <Sidebar/>
+              </div>
+            </Col>
+            <Col>
+                <div className="ButtonsContainer">
+                    {/* <button className="PageButton" onClick={handleStartGame}>Game</button>
+                    <button className="PageButton" onClick={handleSettings}>Settings</button>
+                    <button className="PageButton" onClick={handleLogout}>Logout</button> */}
+                <button className="PageButton" onClick={startGame}>
+                  <div className="ButtonContent">
+                  <img src={startGameImage} alt="Start Game" style={{ width: '20vw', height: '260px' }} />
+                    <div className="ButtonText">Start Game</div>
+                  </div>
+                </button>
+                <button className="PageButton" onClick={pastGame}>
+                  <div className="ButtonContent">
+                  <img src={startGameImage} alt="Start Game" style={{ width: '20vw', height: '260px' }} />
+                    <div className="ButtonText">Replay Past Games</div>
+                  </div>
+                </button>
+                <button className="PageButton" onClick={leaderBoard}>
+                  <div className="ButtonContent">
+                  <img src={leaderBoardImage} alt="Start Game" style={{ width: '20vw', height: '270px' }} />
+                    <div className="ButtonText">Leader Board</div>
+                  </div>
+                </button>
+              </div>
+            </Col>
+            </Row>
+        </Container>
     </div>
   );
 }
