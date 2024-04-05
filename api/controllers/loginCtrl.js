2b5430078bf0f27c1a8108d852d199d1c777a189
VOXXXX1874 <1155173938@link.cuhk.edu.hk>
2024-04-05 15:04:24 +0800

    db controller and test function for login

--- api/controllers/loginCtrl.js
+++ api/controllers/loginCtrl.js
@@ -1,9 +1,19 @@
+const getUserInformation = require('../controllers/dbControllers/loginDbCtrl')
+
+async function getUserInformation_test(username){
+    return Array(
+        {password: '123456'}
+    );
+}
+
 // This controller is called by the router, ask database controller for user information, and send the user information back
 async function verifyLoginInformation(req,res){
     // Read the username and password from the request
     const {username,password} = req.body;
     console.log('Username:',username);
     console.log('Password:',password);
+    resFromDb = await getUserInformation_test(username)
+    console.log('response from db:',resFromDb)
     // TO DO: call dbController to get the account information to verify the username and password.
     // Always return true
     res.status(200).json({ message: 'Successfully login' });
