4550583bd6c06c2158c7bc95881ec8f8db90249c
Lana McKay <lanamckay@Lanas-MacBook-Air.local>
2024-04-10 17:19:39 +0800

    manage account

--- client/src/pages/ManageProfile/index.jsx
+++ client/src/pages/ManageProfile/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom'; 
 import Header from '../../components/Header/Header';
+import Button from 'react-bootstrap/Button';
+import usernameIcon from './username.png'
+import passwordIcon from './password.png'
+import './index.css'
 
 export default function ManageProfilePage(){
-    // I only write a head to test the setting page. Feel free to delete the head.
-    return(
-        <div className="<Manage Profile Page>">
-             <Header></Header>
-        </div>
+    return (
+        <Router>
+            <div className="ManageProfilePage">
+                <Header />
+                <h1>Profile</h1>
+                <div className="button-container">
+                    <Button variant="light">
+                        <img src={usernameIcon} style={{ width: '20px', height: '20px' }} /> Change username
+                    </Button>
+                    <Button variant="light">
+                        <img src={passwordIcon} style={{ width: '20px', height: '20px' }} /> Change password
+                    </Button>
+                </div>
+            </div>
+        </Router>
     );
+    
 }
+
